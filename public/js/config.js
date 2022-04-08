d7ba531bbb5ba0ee4bb7cb59e6b6cf34fbeb5772
wr <2524359505@qq.com>
2022-04-08 11:18:28 +0800

    优化

--- public/js/config.js
+++
@@ -1,9 +0,0 @@
-var Config = {
-    serverWSPort: '8002',
-    serverAddress: "sdkserver.cloudicp.huawei.com",
-    serverHttpPort: "8002",
-    debugMode: "true",
-    ringFlag: "0",
-    mode: "window",
-    ssl_enable: true
-}
